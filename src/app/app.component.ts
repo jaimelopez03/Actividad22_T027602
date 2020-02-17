293156ff4a324f261adeb749e21022a159aa8337
jaimelopez03 <60837707+jaimelopez03@users.noreply.github.com>
2020-02-16 23:40:19 -0800

    Actividad Finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare let $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'Actividad-Cash-Tracking';
+export class AppComponent implements OnInit {
+  constructor() {}
+  total = null;
+  transactions = [];
+  ngOnInit() {
+    $('.modal').modal();
+  }
+  incomeamount(myInput: HTMLInputElement): void {
+    const value = myInput.value;
+    if (value === '' || value < '0') {
+      alert('Ingresar un valor NUMERICO mayor a 0');
+    } else {
+      this.transactions.unshift(value);
+      this.total = this.total + (+myInput.value);
+      myInput.value = '';
+    }
+  }
+  expenseamount(myInput: HTMLInputElement): void {
+    const value = myInput.value;
+    if (value === '' || value < '0') {
+      alert('Ingresar un valor NUMERICO mayor a 0');
+    } else {
+      this.transactions.unshift(value);
+      this.total = this.total - (+myInput.value);
+      myInput.value = '';
+    }
+  }
+
 }
